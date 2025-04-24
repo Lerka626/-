1d2439510a6c52e1659384a5d5b95d36649ee8bb
Lerka626 <168011659+Lerka626@users.noreply.github.com>
2025-04-24 13:09:23 +0500

    Add files via upload

---
+++ animals_30vid.py
@@ -0,0 +1,5 @@
+animals_30vid = {"argali": "архар", "Asian-badger": "азиатский барсук", "badger": "барсук", "beaver": "бобр", "brown-bear": "бурый медведь", "capercaillie": "глухарь", 
+                 "crane": "журавль", "field-lark": "полевой жаворонок", "fox": "лиса", "gold-eagle": "беркут", "Groundhog-baibak": "сурок-байбак", "hare": "заяц", 
+                 "heron": "цапля", "hog": "кабан", "kedrovka": "кедровка", "lynx": "рысь", "mallard": "кряква", "marten": "куница", "mericanmink": "американская норка", 
+                 "moose": "лось", "murtiger": "тигр", "otter": "выдра", "polecat": "хорёк", "raccoon-dog": "енотовидная собака", "sub-deer": "косуля", "swan": "лебедь", 
+                 "ular": "улар", "white-tailed-eagle": "орлан-белохвост", "wolf": "волк", "woodpecker": "дятел"}
