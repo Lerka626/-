87c63aa54eedf328db4ddbd98d0b3d5354f6e48b
Andrey Maslov <146727412+AdneryMaslov@users.noreply.github.com>
2025-06-19 04:07:10 +0500

    add text/ Docker

--- fullstack/frontend/my-react-app/src/components/ProcessPhoto/ProcessPhoto.jsx
+++ fullstack/frontend/my-react-app/src/components/ProcessPhoto/ProcessPhoto.jsx
@@ -9,7 +9,12 @@ export default function ProcessPhoto() {
     if (params.results === 0) {
         return <Filters setParams={setParams} />
     } else if (params.results === 'load') {
-        return <div className='loadText'>Подождите, идет загрузка и обработка...</div>
+        return (
+            <div className='loading-overlay'>
+                <div className='loader'></div>
+                <div className='loadText'>Подождите, идет загрузка и обработка...</div>
+            </div>
+        )
     } else {
         return <Result params={params} setParams={setParams} />
     }
